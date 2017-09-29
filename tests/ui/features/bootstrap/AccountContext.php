24a545adfb2e57fcecad6c3d50547b5719513bb1
Phil Davis <phil@jankaritech.com>
2017-09-29 14:21:17 +0545

    Change account first and last name

---
+++ tests/ui/features/bootstrap/AccountContext.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+use Behat\Behat\Context\Context;
+use Behat\MinkExtension\Context\RawMinkContext;
+use Page\AccountPage;
+
+require_once 'bootstrap.php';
+
+/**
+ * Assets context.
+ */
+class AccountContext extends RawMinkContext implements Context
+{
+    private $accountPage;
+
+    public function __construct(AccountPage $page)
+    {
+        $this->accountPage = $page;
+    }
+
+    /**
+     * @Given I am on the account page
+     */
+    public function iAmOnTheAccountPage()
+    {
+        $this->accountPage->open();
+    }
+
+    /**
+     * @Given I go to the account page
+     */
+    public function iGoToTheAccountPage()
+    {
+        $this->visitPath($this->accountPage->getPagePath());
+        $this->accountPage->waitForOutstandingAjaxCalls($this->getSession());
+    }
+    /**
+     * @When I set account first name to :firstName
+     */
+    public function iSetAccountFirstNameTo($firstName)
+    {
+        $this->accountPage->setFirstName($firstName);
+    }
+
+    /**
+     * @When I set account last name to :lastName
+     */
+    public function iSetAccountLastNameTo($lastName)
+    {
+        $this->accountPage->setLastName($lastName);
+    }
+
+    /**
+     * @When I apply the account changes
+     */
+    public function iApplyTheAccountChanges()
+    {
+        $this->accountPage->applyAccountChanges();
+    }
+
+    /**
+     * @Then the account first name is :firstName
+     */
+    public function theAccountFirstNameIs($firstName)
+    {
+        PHPUnit_Framework_Assert::assertEquals(
+            $firstName,
+            $this->accountPage->getFirstName()
+        );
+    }
+
+    /**
+     * @Then the account last name is :lastName
+     */
+    public function theAccountLastNameIs($lastName)
+    {
+        PHPUnit_Framework_Assert::assertEquals(
+            $lastName,
+            $this->accountPage->getLastName()
+        );
+    }
+}
--- tests/ui/features/lib/AccountPage.php
+++ tests/ui/features/lib/AccountPage.php
@@ -11,6 +11,9 @@
 
 namespace Page;
 
+use SensioLabs\Behat\PageObjectExtension\PageObject\Exception\ElementNotFoundException;
+use WebDriver\Exception\ElementNotVisible;
+
 class AccountPage extends MauticPage
 {
     /**
@@ -18,4 +21,75 @@ class AccountPage extends MauticPage
      * @var string $path
      */
     protected $path = '/s/account';
+    protected $firstNameInputId = "user_firstName";
+    protected $lastNameInputId = "user_lastName";
+    protected $applyButtonNormalId = "user_buttons_save_toolbar";
+    protected $applyButtonMobileId = "user_buttons_save_toolbar_mobile";
+
+    public function setFirstName($firstname)
+    {
+        $this->fillField($this->firstNameInputId, $firstname);
+    }
+
+    public function setLastName($lastname)
+    {
+        $this->fillField($this->lastNameInputId, $lastname);
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        $firstNameElement = $this->findById($this->firstNameInputId);
+
+        if ($firstNameElement === null) {
+            throw new ElementNotFoundException(
+                "getFirstName:could not find element"
+            );
+        }
+
+        return $firstNameElement->getValue();
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        $lastNameElement = $this->findById($this->lastNameInputId);
+
+        if ($lastNameElement === null) {
+            throw new ElementNotFoundException(
+                "getLastName:could not find element"
+            );
+        }
+
+        return $lastNameElement->getValue();
+    }
+
+    public function applyAccountChanges()
+    {
+        $applyButton = $this->findById($this->applyButtonNormalId);
+
+        if ($applyButton === null) {
+            throw new ElementNotFoundException("could not find normal account apply button");
+        }
+
+        if (!$applyButton->isVisible()) {
+            $applyButton = $this->findById($this->applyButtonMobileId);
+
+            if ($applyButton === null) {
+                throw new ElementNotFoundException("could not find mobile account apply button");
+            }
+        }
+
+        if (!$applyButton->isVisible()) {
+            throw new ElementNotVisible("could not find any visible account apply button");
+        }
+
+        $applyButton->click();
+    }
 }
